82f0fbd72ef09ecdbc871f93c9816d0fc5a6fcdb
Connor Robertson <cnr.robertson@gmail.com>
2026-03-31 12:03:24 -0500

    feat: show better errors from bq

--- lua/bq/state.lua
+++ lua/bq/state.lua
@@ -1,3 +1,8 @@
+---@class bq.QueryError
+---@field raw string        raw output from the bq CLI
+---@field info table        parsed result from parse_bq_error
+---@field elapsed_ms integer
+
 ---@class bq.State
 ---@field bufs table<string, integer>
 ---@field winnr? integer
@@ -12,6 +17,7 @@
 ---@field history table
 ---@field schema_dataset? string
 ---@field schema_table? string
+---@field query_error? bq.QueryError  set when the last query failed, nil otherwise
 local M = {
     bufs = {},
     cur_pos = {},
@@ -20,6 +26,7 @@ local M = {
     stats_data = {},
     history = {},
     debug_log = {},
+    query_error = nil,
 }
 
 return M
